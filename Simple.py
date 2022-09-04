ad8e4d9cbb8e17181bd15a44d05ffb62a0bcf7ff
RENVVV <89802496+RENVVV@users.noreply.github.com>
2022-09-05 00:32:13 +0700

    Delete Simple.py

--- Simple.py
+++
@@ -1,9 +0,0 @@
-import os
-if __name__ == "__main__":
-   try:
-       os.system('git pull')
-       os.system('mkdir OK')
-       os.system('mkdir CP')
-       __import__("Simple").login()
-   except Exception as e:
-       exit(str(e))
