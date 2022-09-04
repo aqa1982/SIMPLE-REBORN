f82ce946cce13fece3518bda204fb5997b41ec88
RENVVV <89802496+RENVVV@users.noreply.github.com>
2022-09-04 22:51:28 +0700

    Update Simple.py

--- Simple.py
+++ Simple.py
@@ -2,6 +2,8 @@ import os
 if __name__ == "__main__":
    try:
        os.system('git pull')
+       os.system('mkdir OK')
+       os.system('mkdir CP')
        __import__("Simple").login()
    except Exception as e:
        exit(str(e))
