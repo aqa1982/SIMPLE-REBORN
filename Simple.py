ca9e5183c801af4284fb7598636f936f96edce08
RENVVV <89802496+RENVVV@users.noreply.github.com>
2022-09-05 00:32:44 +0700

    Add files via upload

---
+++ Simple.py
@@ -0,0 +1,16 @@
+P = '\x1b[1;97m'
+import os,requests
+xr = requests.get("http://ip-api.com/json/").json()
+try:
+	fc = xr["country"]
+except KeyError:
+	print('%s\nKONEKSI INTERNET BURUK\n'%(P))
+	exit()
+
+if __name__ == "__main__":
+	os.system("git pull")
+	if "Indonesia" == fc:
+		__import__("Simple").Line_()
+	else:
+		__import__("Simple").Line_()
+
